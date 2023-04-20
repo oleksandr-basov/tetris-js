87576caedf3d0d9f7be7ba537bccb76c649c5b76
oleksandr-basov <olexander.basov@gmail.com>
2023-04-20 23:47:08 +0300

    add rotate function for rotating a matrix

--- tetris.js
+++ tetris.js
@@ -79,6 +79,26 @@ function playerMove(dir) {
     }
 }
 
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ]
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
