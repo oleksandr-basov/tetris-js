7a7cfd931610ac8de3a219ef454ba61b2b7e5f0d
oleksandr-basov <olexander.basov@gmail.com>
2023-04-20 23:24:34 +0300

    add function collide

--- tetris.js
+++ tetris.js
@@ -9,6 +9,20 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                    arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function createMatrix(w, h) {
     const matrix = [];
     while (h--) {
@@ -49,6 +63,11 @@ function merge(arena, player) {
 
 function playerDrop() {
     player.pos.y++;
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
 }
 
